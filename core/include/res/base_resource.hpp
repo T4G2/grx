43bdcee78cd2b40fd83e388842f5d203abd5cf50
T4G2 <492811@mail.muni.cz>
2021-05-03 17:43:23 +0200

    UML diagram updated Manager Revisioned

---
+++ core/include/res/base_resource.hpp
@@ -0,0 +1,20 @@
+/**
+ * @file base_resource.hpp
+ * @author Patrik Baso (492811@mail.muni.cz)
+ * @brief 
+ * @version 0.1
+ * @date 2021-05-03
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <string>
+
+
+class BaseResource {
+    uint32_t resource_id;
+    std::string name;
+    std::string path;
+
+};
